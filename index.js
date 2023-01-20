c59b7b577b11992f512f19e729e64118466401e2
Kimberly Nolsin <kimberly.nolsin@gmail.com>
2023-01-19 23:06:16 -0500

    Handling /videos request

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const videoRoutes = require('./routes/video')
 
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("./public/images"));
+
+app.use('/videos', videoRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
--- routes/video.js
+++ routes/video.js
@@ -1,1 +1,31 @@
- 
+const express = require("express");
+const router = express.Router();
+
+const data = require("../data/videos.json");
+
+router.get("/", (req, res, next) => {
+  const videoList = data.map((video) => {
+    res.send(video.id);
+  });
+
+  // const videoList = data.map((video) => {
+  //   [
+  //     {
+  //       id: video.id,
+  //       title: video.title,
+  //       channel: video.channel,
+  //       image: video.image,
+  //     },
+  //   ];
+  // });
+  // res.json(videoList);
+});
+
+// router.get('/:vidId',(req,res,next)=>{
+//   const videoId = req.params.vidId
+//   const video = data.find(vid =>{
+//     return vid.id === videoId
+//   })
+//   res.send(video)
+// })
+module.exports = router;
